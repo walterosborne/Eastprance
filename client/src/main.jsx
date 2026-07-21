754578a7b44161908484ef87a32d2756fe75ee8e
walterosborne <willow3085@gmail.com>
2026-07-21 13:59:24 -0400

    favicon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
+import { SITE_BRANDING } from './siteBranding';
 import './styles.css';
+
+if (typeof document !== 'undefined' && SITE_BRANDING.iconSrc) {
+  const faviconLink =
+    document.getElementById('app-favicon')
+    ?? document.querySelector('link[rel="icon"]');
+
+  if (faviconLink) {
+    faviconLink.setAttribute('href', SITE_BRANDING.iconSrc);
+
+    if (!faviconLink.getAttribute('type')) {
+      faviconLink.setAttribute('type', 'image/svg+xml');
+    }
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
